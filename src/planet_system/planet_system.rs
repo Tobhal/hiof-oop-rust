bc3a8e3555e4726f63534ce5fcdf4b74b4a2b35f
Tobias Hallingstad <tobias@hallingstad.me>
2023-07-02 16:58:21 +0200

    Removed unused code

--- src/planet_system/planet_system.rs
+++ src/planet_system/planet_system.rs
@@ -50,49 +50,6 @@ impl PlanetSystem {
         }
     }
 
-    /*
-    pub fn new_system_from_file(file_name: String, name: String) -> PlanetSystem {
-        let mut center_star: CenterStar = CenterStar::new();
-        let mut planets: Vec<Planet> = vec![];
-        let mut moons: HashMap<String, Vec<Moon>> = HashMap::new();
-
-        read_lines(file_name).enumerate()
-            .filter(|(i, _)| i.clone() != 0)
-            .filter(|(_, line)| !line.as_ref().unwrap().is_empty())
-            .for_each(|(_, line)| {
-                let line = line.unwrap();
-
-                match PlanetSystemCSV::try_from(line).unwrap().t {
-                    Types::CenterStar(s) => center_star = s,
-                    Types::Planet(p, _) => planets.push(p),
-                    Types::Moon(m, c) => {
-                        let mut moon = m.to_owned();
-                        let new_moon = m.to_owned();
-
-                        moons.entry(c)
-                            .and_modify(|list| list.push(mem::replace(&mut moon, new_moon)))
-                            .or_insert(vec![moon]);
-                    }
-                }
-            });
-
-        planets.iter_mut()
-            .filter(|p| moons.contains_key(&*p.name))
-            .for_each(|p| {
-                moons.get(&*p.name).unwrap().iter()
-                    .for_each(|m| {
-                        p.moons.push(m.clone());
-                    })
-            });
-
-        PlanetSystem {
-            name,
-            center_star,
-            planets,
-        }
-    }
-     */
-
     pub fn new_systems_from_file(file_name: String) -> Vec<PlanetSystem> {
         let mut planets: HashMap<String, Vec<Planet>> = HashMap::new();
         let mut stars: HashMap<String, CenterStar> = HashMap::new();
