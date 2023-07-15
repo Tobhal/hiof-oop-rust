25cb7d47852811fcc62da5c92133a7b96bced925
Tobias Hallingstad <tobias@hallingstad.me>
2023-07-15 23:16:53 +0200

    Added field_editable proc macro derive

---
+++ field-editable-derive/target/debug/deps/field_editable_derive-70b2129505eb2c89.d
@@ -0,0 +1,5 @@
+/Users/tobiashallingstad/Prog/Languages/Rust/hiof-oop-rust/field-editable-derive/target/debug/deps/field_editable_derive-70b2129505eb2c89.rmeta: src/lib.rs
+
+/Users/tobiashallingstad/Prog/Languages/Rust/hiof-oop-rust/field-editable-derive/target/debug/deps/field_editable_derive-70b2129505eb2c89.d: src/lib.rs
+
+src/lib.rs:
---
+++ field-editable-derive/target/debug/deps/unicode_ident-d1851e647e0091ae.d
@@ -0,0 +1,6 @@
+/Users/tobiashallingstad/Prog/Languages/Rust/field-editable/field-editable-derive/target/debug/deps/unicode_ident-d1851e647e0091ae.rmeta: /Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
+
+/Users/tobiashallingstad/Prog/Languages/Rust/field-editable/field-editable-derive/target/debug/deps/unicode_ident-d1851e647e0091ae.d: /Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs /Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs
+
+/Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/lib.rs:
+/Users/tobiashallingstad/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.11/src/tables.rs:
